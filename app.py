50af1edaa4dfc3864321faea9a3d5f19e3d358f1
vinmani2001 <vinmani2001@yahoo.com>
2026-07-28 18:53:33 -0400

    blacak

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return jsonify({"message": "Welcome to the Flask API! version 3.0"})
+    return jsonify({"message": "Welcome to the Flask API! version 1.0"})
 
 
 @app.route("/employees")
 def get_employees():
-    return jsonify([{"id": 1, "name": "John"}, {"id": 2, "name": "Manisha"}])
+    return jsonify([{"id": 1, "name": "John"}, {"id": 2, "name": "Mary"}])
 
 
 if __name__ == "__main__":
